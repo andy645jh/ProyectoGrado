5aeb0ba66ef0c210fc543ef88a1315bd45be1617
DESKTOP-H7EOGC9\User <sandypau206@gmail.com>
2017-02-05 20:17:11 -0500

    crud de act misional listo

--- ProyectoGrado/build/generated-sources/ap-source-output/com/proyecto/persistences/Convenciones_.java
+++ ProyectoGrado/build/generated-sources/ap-source-output/com/proyecto/persistences/Convenciones_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-08T21:08:18")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-02-05T14:36:52")
 @StaticMetamodel(Convenciones.class)
 public class Convenciones_ { 
 
---
+++ ProyectoGrado/src/java/com/proyecto/controllers/CoordinacionController.java
@@ -0,0 +1,87 @@
+package com.proyecto.controllers;
+
+import com.proyecto.facades.CoordinacionFacade;
+import com.proyecto.persistences.Coordinacion;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.context.RequestContext;
+
+
+@ManagedBean
+@SessionScoped
+public class CoordinacionController {
+
+    @EJB
+    private CoordinacionFacade _ejbFacade;
+    
+    private Coordinacion _obj;
+    
+    private FacesMessage message;
+    
+    public CoordinacionController() {
+    }
+    
+    public Coordinacion getCampo()
+    {
+        if(_obj==null)  _obj= new Coordinacion();
+        return _obj;        
+    }
+    
+    public void resetear()
+    {
+        _obj = null;
+    }
+    
+    public List<Coordinacion> getListado()
+    {
+        return _ejbFacade.listado();
+    }
+    
+    public void mostrarMensaje()
+    {        
+        if(message!=null) FacesContext.getCurrentInstance().addMessage("mensajes", message);
+        message=null;
+    }
+    
+    public void abrirCrear() {
+        Map<String,Object> options = new HashMap<String, Object>();
+        options.put("resizable", false);
+        options.put("draggable", false);
+        options.put("modal", true);
+        RequestContext.getCurrentInstance().openDialog("/coordinacion/crear", options, null);
+    }
+    
+    public void agregar()
+    {
+        String titulo,detalle;
+        
+        try {
+            titulo = ResourceBundle.getBundle("/com/proyecto/utilities/GeneralTxt").getString("exitoso");
+            detalle = ResourceBundle.getBundle("/com/proyecto/utilities/GeneralTxt").getString("guardaExitoso");
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO,titulo,detalle);
+           
+            _ejbFacade.crear(_obj);           
+            RequestContext context = RequestContext.getCurrentInstance();
+            context.closeDialog(null);
+        } catch (Exception e) 
+        {
+            titulo = ResourceBundle.getBundle("/com/proyecto/utilities/GeneralTxt").getString("error");
+            detalle = ResourceBundle.getBundle("/com/proyecto/utilities/GeneralTxt").getString("guardarError");
+            message = new FacesMessage(FacesMessage.SEVERITY_ERROR,titulo,detalle);
+            RequestContext context = RequestContext.getCurrentInstance();
+            context.closeDialog(null);
+            
+            Logger.getLogger(Coordinacion.class.getName()).log(Level.SEVERE,null,e);
+            
+        }
+    }
+}
