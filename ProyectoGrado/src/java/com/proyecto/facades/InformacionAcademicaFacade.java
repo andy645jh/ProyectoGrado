621ea3b5aac98a758be17d7ceaaf1530e1e11171
DESKTOP-H7EOGC9\User <sandypau206@gmail.com>
2017-06-04 10:35:03 -0500

    perfil

---
+++ ProyectoGrado/src/java/com/proyecto/facades/InformacionAcademicaFacade.java
@@ -0,0 +1,75 @@
+
+package com.proyecto.facades;
+
+import com.proyecto.persistences.Actividades;
+import com.proyecto.persistences.Docentes;
+import com.proyecto.persistences.InformacionAcademica;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+
+@Stateless
+public class InformacionAcademicaFacade extends AbstractFacade<InformacionAcademica>{
+    
+    @EJB
+    private DocentesFacade docentesFacade;
+
+    @PersistenceContext(unitName = "ProyectoGradoPU")
+    private EntityManager _em;
+    
+    
+    public InformacionAcademicaFacade()
+    {
+        super(InformacionAcademica.class);
+    }
+
+    @Override
+    protected EntityManager obtenerEntidad() 
+    {
+        return _em;
+    }   
+    
+    
+    public List<InformacionAcademica> buscarCampo(String columna,String valorBuscar)
+    {
+        CriteriaBuilder cb= obtenerEntidad().getCriteriaBuilder();
+        CriteriaQuery<Actividades> cq= cb.createQuery(Actividades.class);
+        Root<Actividades> objActividades = cq.from(Actividades.class);
+        
+        if(!valorBuscar.equals("") && !columna.equals(""))
+        {            
+            if(columna.equals("cod_docente"))
+            {
+                Join<Actividades,Docentes> doc = objActividades.join("cod_docente");
+                Expression<String> valor = doc.get("_cedula");
+                String cadena = valorBuscar;
+                Predicate condicion = cb.equal(valor, cadena);                
+                cq.where(condicion);
+            }else{           
+       
+                System.out.println("Columna: " + columna);
+                Expression<String> valorCampo = objActividades.get(columna);
+                String cadena = valorBuscar;
+                Predicate condicion = cb.equal(valorCampo, cadena);
+                cq.where(condicion);               
+            }
+            
+            
+        }else{
+            cq.from(InformacionAcademica.class);                     
+        }
+        
+        Query consulta = _em.createQuery(cq);
+        return consulta.getResultList();
+    }
+}
---
+++ ProyectoGrado/src/java/com/proyecto/persistences/InformacionAcademica.java
@@ -0,0 +1,191 @@
+package com.proyecto.persistences;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "info_academica")
+public class InformacionAcademica implements Serializable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codcademico")
+    private int codcademico;
+    
+    @Column(name = "nivel")
+    @Size(min = 1, max = 3)
+    @NotNull
+    private String nivel;
+    
+    @Column(name = "titulo")    
+    @NotNull
+    private String titulo;
+    
+    @Column(name = "institucion")
+    @Size(min = 1, max = 100)
+    @NotNull
+    private String institucion;
+       
+    @JoinColumn(name = "cod_docente", referencedColumnName = "cedula")
+    @ManyToOne(optional = false)
+    private Docentes cod_docente;    
+        
+    @Column(name = "horas")   
+    @NotNull
+    private double _horas;
+    
+    @Column(name = "tipo")   
+    private int tipo;
+    
+    @Column(name = "registro")    
+    @NotNull
+    private String registro;
+    
+    @Column(name = "pais")    
+    @NotNull
+    private String pais;
+    
+     @Column(name = "ano_grado")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date ano_grado;
+  
+
+    public InformacionAcademica() {  }
+
+    public InformacionAcademica(int codcademico, String nivel, String titulo, String institucion, Docentes cod_docente, double _horas, int tipo, String registro, String pais, Date ano_grado) {
+        this.codcademico = codcademico;
+        this.nivel = nivel;
+        this.titulo = titulo;
+        this.institucion = institucion;
+        this.cod_docente = cod_docente;
+        this._horas = _horas;
+        this.tipo = tipo;
+        this.registro = registro;
+        this.pais = pais;
+        this.ano_grado = ano_grado;
+    }
+
+    public int getCodcademico() {
+        return codcademico;
+    }
+
+    public void setCodcademico(int codcademico) {
+        this.codcademico = codcademico;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getInstitucion() {
+        return institucion;
+    }
+
+    public void setInstitucion(String institucion) {
+        this.institucion = institucion;
+    }
+
+    public Docentes getCod_docente() {
+        return cod_docente;
+    }
+
+    public void setCod_docente(Docentes cod_docente) {
+        this.cod_docente = cod_docente;
+    }
+
+    public double getHoras() {
+        return _horas;
+    }
+
+    public void setHoras(double _horas) {
+        this._horas = _horas;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(String registro) {
+        this.registro = registro;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public Date getAno_grado() {
+        return ano_grado;
+    }
+
+    public void setAno_grado(Date ano_grado) {
+        this.ano_grado = ano_grado;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + this.codcademico;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final InformacionAcademica other = (InformacionAcademica) obj;
+        if (this.codcademico != other.codcademico) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "InformacionAcademica{" + "titulo=" + titulo + '}';
+    }
+
+    
+    
+}
+
+    
