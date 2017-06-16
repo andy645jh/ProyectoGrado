5b38f283e84365db4b4006463de37a4f7182e2a3
andy jhonson <andy645jh@gmail.com>
2017-06-16 13:23:16 -0500

    Se arreglo el logout Se agrego la opcion de clear para la session

--- ProyectoGrado/src/java/com/proyecto/utilities/SessionUtils.java
+++ ProyectoGrado/src/java/com/proyecto/utilities/SessionUtils.java
@@ -14,11 +14,18 @@ import javax.faces.context.FacesContext;
 public class SessionUtils
 {
 
-    public static void add(String key, Object value) {
-        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(key, value);
+    public static void clear()
+    {
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().clear();
+    }
+    
+    public static void add(String key, Object value) 
+    {
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(key, value);        
     }
 
-    public static Object get(String key) {
+    public static Object get(String key) 
+    {
         return FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(key);
     }
 }
