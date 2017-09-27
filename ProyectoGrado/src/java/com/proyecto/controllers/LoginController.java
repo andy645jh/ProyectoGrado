05db9346a64e580374d09e5cc3987fbf2ef037ef
Sandy Carecula Cala <=>
2017-09-27 14:11:37 -0500

    rol auditor

--- ProyectoGrado/src/java/com/proyecto/controllers/LoginController.java
+++ ProyectoGrado/src/java/com/proyecto/controllers/LoginController.java
@@ -93,9 +93,9 @@ public class LoginController implements Serializable {
                 SessionUtils.add("docente", doc);
                 SessionUtils.add("coordinacion", doc.getCodcoordinacion());
                 return "/actividades/listado?faces-redirect=true";
-            } else if (request.isUserInRole("evaluador")) {
-                System.out.println("EVALUADOR");
-                return "index_evaluador?faces-redirect=true";
+            } else if (request.isUserInRole("auditor")) {
+                System.out.println("AUDITOR");
+                return "index_auditor?faces-redirect=true";
             }
 
             return "index";
