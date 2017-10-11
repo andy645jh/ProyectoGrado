894881f16e6f8c90c3b02316b4fcdbfbad0adc50
Elkin-btg <elkin@btg.com.co>
2017-10-11 17:16:22 -0500

    Se valida que hayan datos del horario

--- ProyectoGrado/src/java/com/proyecto/controllers/HorarioController.java
+++ ProyectoGrado/src/java/com/proyecto/controllers/HorarioController.java
@@ -98,6 +98,8 @@ public class HorarioController implements Serializable{
     private void organizarListas()
     {
         List<Horario> listHorario = getListado();
+        if(listHorario.size()<=0) return;
+        
         _objHorario = listHorario.get(0);   
         _arrayInterval = new Intervalo[_intervalos.length];
         System.out.println("TAMAÑO "+listHorario.size());
